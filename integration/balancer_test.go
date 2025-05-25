65f4420a607cf4abc0a1afeb78493d8d452a60ad
namanica <veronichkapilipna@gmail.com>
2025-05-25 21:07:55 +0300

    added balancer integration test

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -19,14 +19,33 @@ func TestBalancer(t *testing.T) {
 		t.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
+	serverSet := make(map[string]bool)
+
+	for i := 0; i < 20; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		if err != nil {
+			t.Fatalf("Request failed: %v", err)
+		}
+
+		lbFrom := resp.Header.Get("lb")
+		if lbFrom == "" {
+			t.Fatalf("Missing lb-from header in response")
+		}
+		serverSet[lbFrom] = true
+		resp.Body.Close()
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
+	if len(serverSet) < 2 {
+		t.Errorf("Expected responses from at least 2 servers, got: %v", serverSet)
+	}
 }
 
 func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+	for i := 0; i < b.N; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		if err != nil {
+			b.Fatalf("Request failed: %v", err)
+		}
+		resp.Body.Close()
+	}
 }
