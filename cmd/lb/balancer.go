6c0f93a9d6e01e838a2c6f04ab3258e4a0ac1612
soylltari <mmmsribranets@gmail.com>
2025-05-25 19:57:43 +0300

    Update module path and import statements to reflect correct repository structure

--- cmd/lb/balancer.go
+++ cmd/lb/balancer.go
@@ -11,8 +11,8 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/roman-mazur/architecture-practice-4-template/httptools"
-	"github.com/roman-mazur/architecture-practice-4-template/signal"
+	"github.com/Homa4/architecture-practice-4-template/httptools"
+	"github.com/Homa4/architecture-practice-4-template/signal"
 )
 
 var (
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/roman-mazur/architecture-practice-4-template
+module github.com/Homa4/architecture-practice-4-template
 
 go 1.24
